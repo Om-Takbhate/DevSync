aff9a794a2cb829e0370edb6d81d44aa59038c72
Om Takbhate <omtakbhate007@gmail.com>
2025-01-19 11:15:51 +0530

    feed api implementation -> GET /user/feed

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const userAuth = require('../middlewares/auth.js')
 const {ConnectionRequest} = require('../models/connectionRequest.js')
+const { User } = require('../models/user.js')
 const userRouter = express.Router()
+
+const USER_SAFE_DATA = ["firstName","lastName","gender","age","photoUrl","about","skills"]
+
 
 //get all the pending requests of the loggedin user
 userRouter.get('/user/requests/recieved',userAuth, async (req,res) => {
@@ -10,7 +14,7 @@ userRouter.get('/user/requests/recieved',userAuth, async (req,res) => {
         const requests = await ConnectionRequest.find({
             toUserId : loggedInUser._id , 
             status : "interested"
-        }).populate("fromUserId",["firstName" , "lastName","gender","age","photoUrl","about","skills"])
+        }).populate("fromUserId",USER_SAFE_DATA)
         res.send({count : requests.length , message : requests})
     }
     catch(err) {
@@ -33,8 +37,8 @@ userRouter.get('/user/connections',userAuth,async (req,res)=>{
                 }
             ]
         }).
-        populate("fromUserId",["firstName","lastName","age","gender","about","skills","photoUrl"])
-        .populate("toUserId",["firstName","lastName","age","gender","about","skills","photoUrl"])
+        populate("fromUserId",USER_SAFE_DATA)
+        .populate("toUserId",USER_SAFE_DATA)
 
 
         //we just need data of users that are connected to loggedIn user
@@ -50,6 +54,45 @@ userRouter.get('/user/connections',userAuth,async (req,res)=>{
         })
         
         res.send({count: data.length , data})
+    }
+    catch(err) {
+        res.status(400).send({error : err.message})
+    }
+})
+
+userRouter.get('/user/feed',userAuth,async(req,res)=>{
+    try {
+        //user must not get his profile in feed
+        //user must not get his connections profiles in feed
+        //user must not get profile who have sent them request or of users to whom logged in user has sent the request
+
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or : [
+                {fromUserId : loggedInUser._id},
+                {toUserId : loggedInUser._id}
+            ]
+        }).select("fromUserId , toUserId ")
+
+        
+
+        const hiddeUsersFromFeed = new Set()
+        connectionRequests.forEach(request => {
+            hiddeUsersFromFeed.add(request.fromUserId)
+            hiddeUsersFromFeed.add(request.toUserId)
+        })
+        
+        
+        //adding logged in users _id , so that user's profile will not be shown in feed data
+        hiddeUsersFromFeed.add(loggedInUser._id)
+
+        //finding users whose _id is not present in hiddenUsersFromFeed
+        const feedData = await User.find({
+            _id : {$nin : Array.from(hiddeUsersFromFeed)}
+        }).select(USER_SAFE_DATA)
+
+
+        res.send({count : feedData.length , data : feedData})
     }
     catch(err) {
         res.status(400).send({error : err.message})
