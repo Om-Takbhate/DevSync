0930699a14058ea53d471967b284fa190d17a1dd
Om Takbhate <omtakbhate007@gmail.com>
2025-03-30 10:12:08 +0530

    some change

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: 'https://devsync-ui.onrender.com/', // ✅ Allow frontend origin
+    origin: 'https://devsync-ui.onrender.com', // ✅ Allow frontend origin
     credentials: true
 }));
 
