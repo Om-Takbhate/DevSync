538a055ed034cc3c86fb7ecf32357fb8c2aea17f
dev-om-7 <om.takbhate@mitcorer.edu.in>
2025-05-25 09:02:25 +0530

    resolving cors

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors({
-    origin: ["https://devsync-ui.onrender.com", "http://localhost:5173"] ,
-    credentials: true // ✅ Required for sending cookies
+    origin: "https://devsync-ui.onrender.com",
+    credentials: true
 }));
 
 app.use(express.json())
